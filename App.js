4df7910a990c2b40d23b4f6ca57ceec4715ab856
CRIMSON <olueatoworosanwo@gmail.com>
2022-01-07 04:22:52 +0100

    Preparing folder for new project

--- App.js
+++ App.js
@@ -2,5 +2,5 @@ import React from "react";
 import { View, Text } from "react-native";
 import Welcome from "./App/Screen/Welcome";
 export default function App() {
-    return <Welcome />;
+    return <View></View>;
 }
