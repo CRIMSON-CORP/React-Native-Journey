8a33a18fc69a97af4e7568d90727684d265c2571
CRIMSON <olueatoworosanwo@gmail.com>
2022-01-28 14:30:07 +0100

    Native Base initial commit

--- App.js
+++ App.js
@@ -1,11 +1,4 @@
 import React from "react";
-import AppContainer from "./AppContainer";
-import Navigator from "./Navigator";
-import Login from "./screens/Login";
 export default function App() {
-    return (
-        <AppContainer>
-            <Login />
-        </AppContainer>
-    );
+    return <View></View>;
 }
